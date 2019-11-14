c57cf0db02a670e41cd421a7cd3890ee1390dcca
kinghow <kinghow@hotmail.com>
2019-11-14 10:18:48 +0800

    new

---
+++ sql_chart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+report_base=/root/report
+cd $report_base
+/usr/local/bin/python3 $report_base/sql_chart.py -p $report_base/dbset.ini
+/usr/local/bin/python3 $report_base/SendEmail.py -p $report_base/emailset.ini -f $report_base/my_chart1.html
